9a05143560df40fad44974f65d4dcd15669469a3
Laura Reale <laura.reale@studentvikarie.se>
2019-09-29 20:17:33 +0200

    refactor imports and exports with es6 syntax

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,10 +1,10 @@
-const React = require('react');
-const PropTypes = require('prop-types');
-const queryString = require('query-string');
-const api = require('../utils/api');
-const Link = require('react-router-dom').Link;
-const PlayerPreview = require('./PlayerPreview');
-const Loading = require('./Loading');
+import React from 'react'
+import PropTypes from 'prop-types'
+import queryString from 'query-string'
+import api from '../utils/api'
+import { Link } from 'react-router-dom'
+import PlayerPreview from './PlayerPreview'
+import Loading from './Loading'
 
 function Profile ({ info }) {
   return (
@@ -53,7 +53,7 @@ class Results extends React.Component {
     }
   }
   componentDidMount() {
-    var { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
+    const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
 
     api.battle([
       playerOneName,
@@ -107,4 +107,4 @@ class Results extends React.Component {
   }
 }
 
-module.exports = Results;
+export default Results;
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios'
 
 const id = "YOUR_CLIENT_ID";
 const sec = "YOUR_SECRET_ID";
